b7da16c4c1da4943a467487afb53c240de1bec8b
Sergey Belousov <seryozha.belousov@gmail.com>
2023-09-15 11:28:22 +0300

    Initial Commit

---
+++ NatifeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NatifeTest
+//
+//  Created by Сергей Белоусов on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
